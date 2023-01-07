3599d2dadeb706cc5fff0db251dd740d254cd83a
Bobby Owolabi <bobby@bobbyowolabi.com>
2023-01-07 14:39:48 -0800

    improvement of MissingNumber, sorting done in place

--- src/com/owodigi/cyclic/sort/MissingNumber.java
+++ src/com/owodigi/cyclic/sort/MissingNumber.java
@@ -1,18 +1,28 @@
 package com.owodigi.cyclic.sort;
 
+/**
+ * The key is that you have to ignore the value N when sorting the numbers
+ */
 class MissingNumber {
 
+    private static void swap(final int a, final int b, final int[] nums) {
+        final int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
     public static int findMissingNumber(final int[] nums) {
-        final int[] sortedNums = new int[nums.length + 1];
         for (int i = 0; i < nums.length; ++i) {
-            sortedNums[nums[i]] = nums[i];
+            while (nums[i] < nums.length && nums[i] != i) {
+                swap(i, nums[i], nums);
+            }
         }
-        for (int i = 0; i < sortedNums.length; ++i) {
-            if (sortedNums[i] != i) {
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != i) {
                 return i;
             }
         }
-        return -1;
+        return nums.length;
     }
 
     public static void main(String[] args) {
