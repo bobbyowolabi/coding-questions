f83695107094e119909f66eca80b6b8710588afc
Bobby Owolabi <bobby@bobbyowolabi.com>
2023-01-19 22:10:28 -0800

    progress check in

--- src/com/owodigi/com/owodigi/depth/first/search/SumOfPathNumbers.java
+++ src/com/owodigi/com/owodigi/depth/first/search/SumOfPathNumbers.java
@@ -40,13 +40,13 @@ class SumOfPathNumbers {
     }
 
     public static void main(String[] args) {
-         TreeNode root = new TreeNode(1);
-         root.left = new TreeNode(0);
-         root.right = new TreeNode(1);
-         root.left.left = new TreeNode(1);
-         root.right.left = new TreeNode(6);
-         root.right.right = new TreeNode(5);
-         System.out.println("Total Sum of Path Numbers: " + SumOfPathNumbers.findSumOfPathNumbers(root));
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(1);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(5);
+        System.out.println("Total Sum of Path Numbers: " + SumOfPathNumbers.findSumOfPathNumbers(root));
 
         root = new TreeNode(1);
         root.left = new TreeNode(7);
