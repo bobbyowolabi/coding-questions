ad4a1bce8d23ab1251f8a69880d8d4c9abc9d3e3
Bobby Owolabi <bobby@bobbyowolabi.com>
2023-01-24 23:44:07 -0800

    Progress check in of BFS problems

---
+++ src/com/owodigi/breadth/first/search/MaxDepth.java
@@ -0,0 +1,10 @@
+package com.owodigi.breadth.first.search;
+
+/**
+ * Title:
+ * Status: NOT SOLVED ❌
+ * Source: https://www.educative.io/courses/grokking-the-coding-interview/3jwVx84OMkO
+ * Problem Statement: Given a binary tree, find its maximum depth (or height).
+ */
+public class MaxDepth {
+}
