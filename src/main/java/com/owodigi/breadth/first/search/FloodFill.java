003b9049bafcfedf174405026858f1c2d8020358
Bobby Owolabi <bobby@bobbyowolabi.com>
2023-03-19 00:32:21 -0700

    Filled in Javadocs for Flood Fill

--- src/main/java/com/owodigi/breadth/first/search/FloodFill.java
+++ src/main/java/com/owodigi/breadth/first/search/FloodFill.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
- * TITLE:
- * LEVEL:
- * STATUS: NOT SOLVED ❌
+ * TITLE: Flood Fill
+ * LEVEL: Easy
+ * STATUS: SOLVED ✅
  * PROBLEM STATEMENT: An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
  * You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
  * To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
